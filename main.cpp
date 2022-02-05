28310cf31b6cf76fa51cf38fd27deb3865a96aac
byronstz <byronsocasi4@gmail.com>
2022-02-05 18:24:06 -0500

    Creando figuras e iconos

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 
 #include <QApplication>
 #include <QTranslator>
-
+//Iconos descargados de https://www.flaticon.es/
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QLocale::setDefault(QLocale::English);
-   QTranslator traduccion;
-    // Solicitando al usuario que seleccione un idioma
-   QStringList idiomas;
-   idiomas << "Ingles"<< "Español";
+    QTranslator traduccion;
+   // Solicitando al usuario que seleccione un idioma
+    QStringList idiomas;
+    idiomas << "Ingles"<< "Español";
     QString idiomaSeleccionado = QInputDialog::getItem(NULL,
                                                        "Idioma",
-                                                      "Seleccione un idioma",
-                                                      idiomas);
+                                                       "Seleccione un idioma",
+                                                       idiomas);
     // Dependiendo del idioma seleccionado, carga el archivo de rtaducción
     if (idiomaSeleccionado == "Ingles"){
         traduccion.load(":/MiPaint_en.qm");
